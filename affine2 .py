f5cd53ec54ce45b533ecd904c882eb4fa24171ea
Arshad Kazi <kaziarshad97@gmail.com>
2019-04-23 16:42:44 +0530

    Affine Cipher Python

--- affine2 .py
+++ affine2 .py
@@ -4,12 +4,16 @@ print("enrypted :")
 encrypt = ""
 decrypt = ""
 
+
 for t in text:
     encrypt += chr((((ord(t)-65+key)%26)) + 65)
 print(encrypt)
 
+
 print("decrypted text: ")
+
 
 for t in encrypt:
     decrypt += chr((((ord(t)-65-key)%26)) + 65)
+
 print(decrypt)
